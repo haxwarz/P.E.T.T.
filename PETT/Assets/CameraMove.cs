158e2809cf021deb2c95cdd30ea6d364c963a205
haxwarz <jhoekman@gmail.com>
2014-02-26 14:26:00 +0100

    Red prototype
    springen, camera boundery control, links rechts bewegen

---
+++ PETT/Assets/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public float leftLimit = -3;
+	public float rightLimit = 1.9f;
+	public float toplimit = 3.5f;
+	public float bottomlimit = 2;
+
+	GameObject mainCamera;
+	// Use this for initialization
+	
+	
+	void Start () {
+		mainCamera = (GameObject) GameObject.FindWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mainCamera.transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftLimit, rightLimit),Mathf.Clamp(transform.position.y, bottomlimit, toplimit),5);
+	}
+}
---
+++ PETT/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Transform groundCheck;
+	private bool grounded = true;
+
+	// Use this for initialization
+	void Start () {
+		groundCheck = transform.Find("groundCheck");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		grounded = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			rigidbody.velocity = new Vector2(2f,rigidbody.velocity.y);
+		}
+		
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			rigidbody.velocity = new Vector2(-2f,rigidbody.velocity.y);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.UpArrow) && grounded){
+			rigidbody.AddForce(new Vector2(0f,250f));
+		}
+	
+	}
+}
