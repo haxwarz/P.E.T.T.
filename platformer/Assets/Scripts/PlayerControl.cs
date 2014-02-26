e6a980aa8315e4cf068df36b9292b1f779c98598
Pieter M <Pee_em_em@hotmail.com>
2014-02-26 13:07:02 +0100

    Platform project added

---
+++ platformer/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	[HideInInspector]
+	public bool jump = false;				// Condition for whether the player should jump.
+
+	public float moveForce = 2f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+	
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private bool grounded = false;			// Whether or not the player is grounded.
+
+	void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
+	}
+
+
+
+
+	void Update()
+	{
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+	}
+
+
+	void FixedUpdate ()
+	{
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		rigidbody2D.velocity = new Vector2 (h * maxSpeed, rigidbody2D.velocity.y);
+
+		// If the player should jump...
+		if(jump)
+		{
+
+			// Add a vertical force to the player.
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+
+			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+			jump = false;
+		}
+	}
+}
