b4ab9e6405bafa569ae42fbd5b60a1d51926ebc5
Pieter M <Pee_em_em@hotmail.com>
2014-03-07 22:42:04 +0100

    rotating test

---
+++ PETT/Assets/Scripts/rotateTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.Rotate(Vector3.up, 90);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(transform.position, -Vector3.up, 90);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.position += transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.position -= transform.forward;
+        }
+	}
+}
