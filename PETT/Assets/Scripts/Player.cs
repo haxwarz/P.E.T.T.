356ca2c069b65ec279c079c6abb5bc4df2687ecd
Pieter M <Pee_em_em@hotmail.com>
2014-03-17 10:18:14 +0100

    New player movement, moved trigger

--- PETT/Assets/Scripts/Player.cs
+++ PETT/Assets/Scripts/Player.cs
@@ -1,83 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	private Transform groundCheck;
-	private bool grounded = false;
-	private bool movable = true;
+    private Transform groundCheck;
+    private bool grounded = false;
+    private bool movable = true;
 
-	public float maxspeed = 5f;
-	public float jumpPower = 250f;
+    public float maxspeed = 5f;
+    public float jumpPower = 250f;
 
-	public Vector3 checkpoint;
+    public Vector3 checkpoint;
 
-	// Use this for initialization
-	void Start () {
-		groundCheck = transform.Find("groundCheck");
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        groundCheck = transform.Find("groundCheck");
 
-		grounded = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
 
-		if (Physics.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("OffWorld"))) {
-			this.transform.position = checkpoint;
-			rigidbody.velocity=new Vector2(0f,0f);
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (Physics.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Checkpoint"))) {
-			checkpoint = this.transform.position;
-		}
+        grounded = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-		if(Input.GetKey(KeyCode.RightArrow) && movable){
-			if(rigidbody.velocity.x < maxspeed){
-				rigidbody.AddForce(new Vector2(15f,0f));
-			}
-			else{
-				rigidbody.velocity=new Vector2(maxspeed, rigidbody.velocity.y);
-			}
-		}
+        if (Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("OffWorld")))
+        {
+            this.transform.position = checkpoint;
+            rigidbody.velocity = new Vector2(0f, 0f);
+        }
+
+        if (Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Checkpoint")))
+        {
+            checkpoint = this.transform.position;
+        }
+
+        /*if(Input.GetKey(KeyCode.RightArrow) && movable){
+            if(rigidbody.velocity.x < maxspeed){
+                rigidbody.AddForce(new Vector2(15f,0f));
+            }
+            else{
+                rigidbody.velocity=new Vector2(maxspeed, rigidbody.velocity.y);
+            }
+        }
 		
-		if(Input.GetKey(KeyCode.LeftArrow) && movable){
-			if(rigidbody.velocity.x > (-maxspeed)){
-				rigidbody.AddForce(new Vector2(-15f,0f));
-			}
-			else{
-				rigidbody.velocity=new Vector2(-maxspeed, rigidbody.velocity.y);
-			}
-		}
-		
-		if(Input.GetKeyDown(KeyCode.UpArrow) && grounded && movable){
-			rigidbody.AddForce(new Vector2(0f,jumpPower));
-		}
+        if(Input.GetKey(KeyCode.LeftArrow) && movable){
+            if(rigidbody.velocity.x > (-maxspeed)){
+                rigidbody.AddForce(new Vector2(-15f,0f));
+            }
+            else{
+                rigidbody.velocity=new Vector2(-maxspeed, rigidbody.velocity.y);
+            }
+        }*/
+        float move = Input.GetAxis("Horizontal");
 
-		if(Input.GetButtonDown("Fire1") && movable){
-			interact();
-		}
-	}
+        Vector3 v = rigidbody.velocity;
+        v.x = move * maxspeed;
+        rigidbody.velocity = v;
 
-	void OnCollisionStay(Collision collisionInfo) {
-		if (collisionInfo.gameObject.tag == "platform" && grounded) {
-			transform.parent = collisionInfo.transform;
-				} else {
-						transform.parent = null;
-				}
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded && movable)
+        {
+            rigidbody.AddForce(new Vector2(0f, jumpPower));
+        }
 
-	}
-	
-	void OnTriggerEnter(Collider collider) {
-		collider.gameObject.GetComponent<EndpointController>().interact();
-		
-	}
+        if (Input.GetButtonDown("Fire1") && movable)
+        {
+            interact();
+        }
+    }
 
-	public void interact(){
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "platform" && grounded)
+        {
+            transform.parent = collisionInfo.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+    }
+
+    public void interact()
+    {
         print("interact");
-		RaycastHit hitInfo;
-		Vector3 dept = transform.TransformDirection(new Vector3(0,0,1));
-		if (Physics.Raycast (transform.position, dept, out hitInfo, 100, 1 << 9)) {
+        RaycastHit hitInfo;
+        Vector3 dept = transform.TransformDirection(new Vector3(0, 0, 1));
+        if (Physics.Raycast(transform.position, dept, out hitInfo, 100, 1 << 9))
+        {
             ComputerController script = hitInfo.collider.gameObject.GetComponent<ComputerController>();
             if (script != null)
             {
@@ -95,8 +108,8 @@ public class Player : MonoBehaviour {
                     movable = false;
                 }
             }
-		}
-	}
+        }
+    }
 
     public void setMovable(bool move)
     {
